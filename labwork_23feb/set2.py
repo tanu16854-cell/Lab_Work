ab2007b0979e8b954ec59049685310b777fcd052
Tanu Rana <tanu16854@gmail.com>
2026-02-23 23:52:35 +0530

    Delete labwork_23feb/set2.py

--- labwork_23feb/set2.py
+++
@@ -1,8 +0,0 @@
-set1 = {45,67,76,34}
-print("First set : ",set1)
-#-----------------------------
-set2 = {67,78,89,85,68}
-print("Second set : ",set2)
-#----------------------------
-#to find the union of two set
-set
