611baafa50a4c10467c274dc4a908a02c15a71cd
Tanu Rana <tanu16854@gmail.com>
2026-02-24 09:47:15 +0530

    Delete labwork_23feb/Lab_Work/Dictionary Program/String Program - Copy directory

--- labwork_23feb/Lab_Work/Dictionary Program/String Program - Copy/Word_count.py
+++
@@ -1,9 +0,0 @@
-# Word Counter
-# This program counts number of words in a sentence
-
-text = input("Enter a sentence: ")
-
-words = text.split()     # split sentence into words
-word_count = len(words)
-
-print("Number of words:", word_count)
