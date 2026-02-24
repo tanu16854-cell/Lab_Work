81a97186cfc2e6f2907ded96f5a52562126e3313
Tanu Rana <tanu16854@gmail.com>
2026-02-24 15:09:07 +0530

    Add files via upload

---
+++ 100 Coding Question/countwordinsentence.py
@@ -0,0 +1,11 @@
+# Count words in sentence
+
+s = input("Enter sentence: ")
+
+words = s.split()
+
+print("Number of words:", len(words))
+
+#output
+#Enter sentence: hello world
+#Number of words: 2
