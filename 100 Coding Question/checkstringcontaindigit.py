708303b594212203dad330c154a2c4e9de29aada
Tanu Rana <tanu16854@gmail.com>
2026-02-24 14:51:04 +0530

    Add files via upload

---
+++ 100 Coding Question/checkstringcontaindigit.py
@@ -0,0 +1,12 @@
+# Check if string contains only digits
+
+s = input("Enter string: ")
+
+if s.isdigit():
+    print("Only digits")
+else:
+    print("Not only digits")
+
+#OUTPUT
+#Enter string: tanu
+#Not only digits
