1792d2d84af49716e2eb4908f9073ec2361e1b69
Tanu Rana <tanu16854@gmail.com>
2026-02-24 13:19:52 +0530

    Add files via upload

---
+++ 100 Coding Question/Multiplication_table.py
@@ -0,0 +1,21 @@
+# Program to print multiplication table
+
+num = int(input("Enter a number: "))
+i = 1
+
+while i <= 10:
+    print(num, "x", i, "=", num * i)
+    i += 1
+    
+#OUTPUT
+#Enter a number:: 3
+#3 x 1 = 3
+#3 x 2 = 6
+#3 x 3 = 9
+#3 x 4 = 12
+#3 x 5 = 15
+#3 x 6 = 18
+#3 x 7 = 21
+#3 x 8 = 24
+#3 x 9 = 27
+#3 x 10 = 30
