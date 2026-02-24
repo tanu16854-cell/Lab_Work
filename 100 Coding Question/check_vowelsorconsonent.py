2f9c96cd060e0a264c77c2a3ccec71bf1a3625d2
Tanu Rana <tanu16854@gmail.com>
2026-02-24 11:57:54 +0530

    Add files via upload

---
+++ 100 Coding Question/check_vowelsorconsonent.py
@@ -0,0 +1,10 @@
+# Program to check vowel or consonant
+
+# Taking input
+ch = input("Enter a character: ").lower()
+
+# Checking condition
+if ch in 'aeiou':
+    print("Vowel")
+else:
+    print("Consonant")
