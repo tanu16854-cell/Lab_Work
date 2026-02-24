3e80bd21270b77f8109a071b7c4d50320a468cf8
Tanu Rana <tanu16854@gmail.com>
2026-02-24 11:31:55 +0530

    Add files via upload

---
+++ 100 Coding Question/convertto_frehntocel.py
@@ -0,0 +1,14 @@
+# Program to convert Fahrenheit to Celsius
+
+# Taking input from user
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Formula: C = (F - 32) × 5/9
+celsius = (fahrenheit - 32) * 5/9
+
+# Display result
+print("Temperature in Celsius:", celsius)
+
+#OUTPUT
+#Enter temperature in Fahrenheit: 64
+#Temperature in Celsius: 17.77777777777778
