836824eb0f00b11824a63c3b413fab456ef6ab12
Tanu Rana <tanu16854@gmail.com>
2026-02-24 11:28:17 +0530

    Add files via upload

---
+++ 100 Coding Question/convertto_celtofrehn.py
@@ -0,0 +1,16 @@
+# Program to convert Celsius to Fahrenheit
+
+# Taking input from user
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Formula: F = (C × 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+
+# Display result
+print("Temperature in Fahrenheit:", fahrenheit)
+
+
+
+#OUTPUT
+#Enter temperature in Celsius: 17
+#Temperature in Fahrenheit: 62.6
