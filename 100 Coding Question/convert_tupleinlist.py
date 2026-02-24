777ebf4e01273c5837bddedbdae38fb21d6e8ee7
Tanu Rana <tanu16854@gmail.com>
2026-02-24 14:30:33 +0530

    Add files via upload

---
+++ 100 Coding Question/convert_tupleinlist.py
@@ -0,0 +1,12 @@
+# Program to convert tuple to list
+
+t = tuple(map(int, input("Enter tuple values: ").split()))
+
+lst = list(t)
+
+print("Converted list:", lst)
+
+
+#Output
+#Enter tuple values: 1 2 3
+#Converted list: [1, 2, 3]
