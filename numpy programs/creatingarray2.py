c87a0d06f5de07a530e8f9b1119f32f6dd49a61e
Tanu Rana <tanu16854@gmail.com>
2026-03-13 11:39:55 +0530

    Add files via upload

---
+++ numpy programs/creatingarray2.py
@@ -0,0 +1,8 @@
+#creating an array of five elements
+import numpy as np
+n1= np.array([1,2,3,4,5])
+print("Array of five elements :")
+print(n1)
+print("------------------------------")
+print("After multiply each emlements by 10 :")
+print(n1*10)
